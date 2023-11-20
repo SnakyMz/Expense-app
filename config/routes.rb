6b7c4c2500a2e846230fcf884d826122e1c6ed05
SnakyMz <Soban.syeed@outlook.com>
2023-11-21 02:58:02 +0530

    Add controllers and basic views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     root "home#index", as: :unauthenticated_root
   end
 
+  authenticated :user do
+    root "groups#index", as: :authenticated_root
+  end
+
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :entities, only: [:index, :new, :create]
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
